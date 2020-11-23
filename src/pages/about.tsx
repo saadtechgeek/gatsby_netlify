c765c3cfbea137e4ff3195a681619c857c285e3c
Saad Shahid <saad.techgeek@gmail.com>
2020-11-23 22:41:17 +0500

    gatsby_netlify

---
+++ src/pages/about.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from "../component/layout";
+
+export default function About() {
+    return <Layout>
+        <h1>This is About page</h1>
+    </Layout>
+}
---
+++ src/pages/contact.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from "../component/layout";
+
+export default function Contact() {
+    return <Layout>
+        <h1>Hello from Contact page</h1>
+    </Layout>
+}
